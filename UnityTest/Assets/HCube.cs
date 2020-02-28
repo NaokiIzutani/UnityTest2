525690c0ab533516659cd3e6139c854c785b0f4e
hoday0202 <kd1242583@st.kobedenshi.ac.jp>
2020-02-28 15:36:25 +0900

    HCube

---
+++ UnityTest/Assets/HCube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HCube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 vel = new Vector3(0.0f, 0.0f, 0.0f);
+        if (Input.GetKey(KeyCode.W)) { vel.z += 0.5f; }
+        if (Input.GetKey(KeyCode.S)) { vel.z -= 0.5f; }
+        if (Input.GetKey(KeyCode.A)) { vel.x -= 0.5f; }
+        if (Input.GetKey(KeyCode.D)) { vel.x += 0.5f; }
+        transform.position += vel;
+    }
+}
